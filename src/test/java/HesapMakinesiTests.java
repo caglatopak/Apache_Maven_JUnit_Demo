ae239d5a4826cf3ae91aca3a44f638ce1e39e8eb
caglatopak <160266658+caglatopak@users.noreply.github.com>
2025-02-09 16:45:18 +0100

    1111

--- src/test/java/HesapMakinesiTests.java
+++ src/test/java/HesapMakinesiTests.java
@@ -1,25 +1,43 @@
 import org.example.HesapMakinesi;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class HesapMakinesiTests {
 
+    HesapMakinesi hesapMakinesi;// nesne olusturuldu
 
-         @Test     // annotaions
+             @BeforeAll // bu annotations ile yazilan methodun hepsi static olmalidir. ayni sekilde @afterall annotations nida static olmalidir
+                        // bu kelime ile testelerden once 1 kez calistirilir
+             public static void setupClass(){
+                 System.out.println("setupClss metodu calistirildi");
+
+             }
+
+             @BeforeEach  // her testten once bu metodun ilk calistirilmasi gerektigini belirtir
+
+                  public void setup(){
+
+                 hesapMakinesi = new HesapMakinesi();
+
+                 System.out.println("setup metodu calistirildi");
+
+    }
+
+
+         @Test     // annotations // bunun bir test oldugunu belirtir
 
               public void toplamaTesti() {
 
-             HesapMakinesi hesapMakinesi = new HesapMakinesi();
+            // hesapMakinesi = new HesapMakinesi();
 
              double toplamaSonucu = hesapMakinesi.toplama(1.5, 3.6);
              Assertions.assertEquals(5.1, toplamaSonucu);
 
          }
+        // @Disabled("cikarma isleminde bug mevcut duzeltme olduktan sonra aktif hale getirilecektir") // bu kelime ile bu test gecici olarak pasif hale getirilmistr
          @Test
-
              public void cikarmaTesti() {
 
-             HesapMakinesi hesapMakinesi = new HesapMakinesi();
+            // hesapMakinesi = new HesapMakinesi();
 
              double cikarmaSonucu = hesapMakinesi.cikarma(10, 5);
              Assertions.assertEquals(5, cikarmaSonucu);
@@ -31,21 +49,38 @@ public class HesapMakinesiTests {
 
               public void carpmaTesti() {
 
-               HesapMakinesi hesapMakinesi = new HesapMakinesi();
+             // hesapMakinesi = new HesapMakinesi();
 
-               double carpmaSonucu = hesapMakinesi.carpma(10, 5);
-               Assertions.assertEquals(50, carpmaSonucu);
+              double carpmaSonucu = hesapMakinesi.carpma(10, 5);
+              Assertions.assertEquals(50, carpmaSonucu);
     }
 
          @Test
 
               public void bolmeTesti() {
 
-              HesapMakinesi hesapMakinesi = new HesapMakinesi();
+             // hesapMakinesi = new HesapMakinesi();
 
               double bolmeSonucu = hesapMakinesi.bolme(10, 5);
               Assertions.assertEquals(2, bolmeSonucu);
     }
 
+
+    @AfterEach // her testen sonra 1 kez calisirilir
+
+    public void teardown (){ // teardown ismi kapatmak anlamina gelir kapanis genelde bu isimle adlandirilir
+
+        System.out.println("teardown metodu calistirildi");
+    }
+
+
+   @AfterAll // tum testlker kosulduktan sonra 1 kez calistirili
+
+    public static void teardownClass(){
+       System.out.println("teardownClass metodu calistirildi");
+   }
+
+
+    // debug = hata ayiklamasi= breakpoint kullanarak orayi calistirip ordaki hatayi bulabiliriz.
 
 }
